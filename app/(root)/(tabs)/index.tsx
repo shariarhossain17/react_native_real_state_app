2b05cf177744547b3eeaafaefec885f16f0fc418
shariar17 <shariarhossain23@gmail.com>
2025-06-06 03:48:50 +0600

    added:nativewind

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,10 +1,11 @@
 import { Link } from "expo-router";
 import React from "react";
-import { View } from "react-native";
+import { Text, View } from "react-native";
 
 const Index = () => {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text className="font-bold text-red-700">Welcome </Text>
       <Link href="/sign-in">Sign in</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Sign in</Link>
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,6 @@
 import { Stack } from "expo-router";
+import "./global.css";
 
 export default function RootLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
-  );
+  return <Stack />;
 }
